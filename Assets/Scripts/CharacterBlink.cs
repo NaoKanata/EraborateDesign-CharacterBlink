c24d01286e97795d1f54f58e3a81ea5f7fcc774b
NaoKanata <kanatanao7o@gmail.com>
2019-04-09 21:08:37 +0900

    Create Comparison Scene

--- Assets/Scripts/CharacterBlink.cs
+++ Assets/Scripts/CharacterBlink.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 
 public class CharacterBlink : MonoBehaviour
 {
+    [SerializeField]
+    Image eyeimage;
 
     [SerializeField]
     Sprite[] sparray;
 
-    int cnt = 0;
-    int blinkcnt = 60;
+    int cnt = 0;    //まばたきするタイミングカウンタ
+    int blinkcnt = 60;  //まばたきするタイミング
+    int aniPos = 0; //アニメーション位置
+    int frate = 6; //アニメーションフレーム
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +31,12 @@ public class CharacterBlink : MonoBehaviour
         if(cnt==0)
         {
             // 瞬きする処理
+            aniPos = frate*sparray.Length;
+        }
+        if(aniPos>0)
+        {
+            aniPos--;
+            eyeimage.sprite = sparray[aniPos/frate];
         }
     }
 }
