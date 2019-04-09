46ceb1a77db963d26db01c77a10ec3e43401a758
NaoKanata <kanatanao7o@gmail.com>
2019-04-09 20:20:16 +0900

    first commit

---
+++ Assets/Scripts/CharacterBlink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class CharacterBlink : MonoBehaviour
+{
+
+    [SerializeField]
+    Sprite[] sparray;
+
+    int cnt = 0;
+    int blinkcnt = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt++;
+        cnt%=blinkcnt;
+        if(cnt==0)
+        {
+            // 瞬きする処理
+        }
+    }
+}
